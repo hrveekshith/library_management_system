ee244a1c1a47dfbbd5d849e1089afbd119f8154c
hrveekshith <veekshithhrveekshithhr@gmail.com>
2025-11-22 18:25:29 +0000

    Initial structure of spring project with maven build

---
+++ src/main/java/com/app/library/book/model/Book.java
@@ -0,0 +1,25 @@
+package com.app.library.book.model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book{
+
+    private Long id;
+    private String title;
+    private  String author;
+    private LocalDate publicationYear;
+    private  Long avaliableCopy;
+    private String genre;
+
+    public String toString(){
+        return "Book Id: "+id
+                +"{Name: "+this.title+", Author: "+this.author+", Publication Year: "+this.publicationYear+", Genre: "+this.genre+", Avaliable Copies: "+this.avaliableCopy+" }\n";
+    }
+}
